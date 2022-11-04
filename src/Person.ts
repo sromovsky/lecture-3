2d5656f4a736e5834b064d093edf13a239620cef
Tomáš Šromovský <tomas.sromovsky@posam.sk>
2022-11-04 13:46:18 +0100

    lecture 3

--- src/Person.ts
+++ src/Person.ts
@@ -1,9 +1,41 @@
 export class Person {
+    private id: number;
     private name: string;
     private age: number;
+    private score: number[];
 
-    constructor(name: string, age: number) {
+    constructor(id: number, name: string, age: number) {
+        this.id = id;
         this.name = name;
         this.age = age;
+        this.score = [];
+    }
+
+    getId(): number {
+        return this.id;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getAge() {
+        return this.age;
+    }
+
+    setName(name: string) {
+        this.name = name;
+    }
+
+    setAge(age: number) {
+        this.age = age;
+    }
+
+    getScore() {
+        return this.score;
+    }
+
+    addScore(value: number) {
+        return this.score.push(value);
     }
 }
