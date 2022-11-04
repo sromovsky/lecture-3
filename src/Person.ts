becf85bff49a596140decb591fbe52c64a5d9ffa
bujdos19 <55790467+bujdos19@users.noreply.github.com>
2022-11-04 14:44:40 +0100

    2

--- src/Person.ts
+++ src/Person.ts
@@ -1,9 +1,13 @@
-export class Person {
+export class Player {
     private name: string;
-    private age: number;
+    private country: string;
+    private rank: number;
+    private score: number;
 
-    constructor(name: string, age: number) {
+    constructor(name: string, country: string, rank: number, score: number) {
         this.name = name;
-        this.age = age;
+        this.country = country;
+        this.rank = rank;
+        this.score = score;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {Request, Response} from 'express';
-import {Person} from './Person';
+import {Player} from './Person';
 
 
 const express = require('express');
@@ -8,14 +8,24 @@ const port = 3000;
 const app = express();
 app.use(express.json());
 
-const array: Person[] = [];
+const CarlosAlcaraz= new Player('Carlos Alcaraz', 'Spain', 1, 6730);
+const RafaelNadal= new Player('Rafael Nadal', 'Spain', 2, 5810);
+const CasperRuud= new Player('Casper Ruud', 'Norway', 3, 5600);
+const DaniilMedvedev= new Player('Daniil Medvedev', 'Russia', 4, 5155);
+const StefanosTsitsipas= new Player('Stefanos Tsitsipas', 'Greece', 5, 4930);
+const AlexanderZverev= new Player('Alexander Zverev', 'Germany', 6, 4860);
+const NovakDjokovic= new Player('Novak Djokovic', 'Serbia', 7, 4320);
+const AndreyRublev= new Player('Andrey Rublev', 'Russia', 8, 3685);
+const TaylorFritz= new Player('Taylor Fritz', 'USA', 9, 3195);
+
+const array: Player[] = [];
 
 app.get('/', (req: Request, res: Response) => {
     res.send(array);
 });
 
 app.post('/', (req: Request, res: Response) => {
-    const value = req.body as Person;
+    const value = req.body as Player;
 
     if (value) {
         const index = array.push(value)
