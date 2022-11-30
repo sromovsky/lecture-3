27e30ceb880c13d662737a02455c78bdd80e1cff
quib963 <55764690+quib963@users.noreply.github.com>
2022-11-30 19:51:46 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import {Request, Response} from 'express';
-import {Person} from './Person';
-
-//test
+import {PlayList} from './PlayList';
 
 const express = require('express');
 const port = 3000;
@@ -9,116 +7,85 @@ const port = 3000;
 const app = express();
 app.use(express.json());
 
-const p1 = new Person(1, "Tomas", 25);
-const p2 = new Person(2, "Filip", 15);
-const p3 = new Person(3, "David", 20);
+const p1 = new PlayList(1, "Martin Garrix" , "The Only Way Is UP" , 3.25,"youtube/441");
+const p2 = new PlayList(2, "Avicii" , "Levels" , 3.20,"youtube/942");
+const p3 = new PlayList(3, "Martin Garrix", "Access" ,3.15,"youtube/264");
+const p4 = new PlayList(4, "Avicii","Hey Brother",4.15,"youtube/765");
+const p5 = new PlayList(5, "Tobu","Cacao",3.12,"youtube/491");
+const p6 = new PlayList(6, "Tobu","Dusk",3.41,"youtube/389");
+const p7 = new PlayList(7, "Tobu","Candyland",3.21,"youtube/547");
+const p8 = new PlayList(8, "Tobu","Enigma",3.41,"youtube/936");
 
-p1.addScore(5);
-p1.addScore(9);
-p1.addScore(10);
-
-p2.addScore(5);
-p2.addScore(15);
-
-p3.addScore(5);
-p3.addScore(9);
-
-let array: Person[] = [p1, p2, p3];
+let array: PlayList[] = [p1, p2, p3, p4, p5, p6, p7, p8];
 
 app.get('/', (req: Request, res: Response) => {
-    res.send({autor: 'Tomas Sromovsky'});
+    res.send({autor: "Jakub Macuha"});
 });
 
-app.get('/users', (req: Request, res: Response) => {
-    const score = Number(req.query.withScore);
-
-    console.log(score);
-
-    let result;
-
-    if (score) {
-        result = array.filter(user => user.getScore().includes(score))
-    } else {
-        result = array;
-    }
-
-    res.send(result.map(user => {
-        return {
-            id: user.getId(),
-            name: user.getName()
-        }
-    }));
+app.get('/PlayList', (req: Request, res: Response) => {
+        res.send(array);
 });
 
-app.get('/users/:id', (req: Request, res: Response) => {
-    const id = Number(req.params.id);
-
-    const user = array.find(user => user.getId() === id);
-    if (user) {
-        res.send(user);
+app.get('/PlayList/:trackId', (req: Request, res: Response) => {
+    const trackId = Number(req.params.trackId);
+    const au = array.find(au => au.getTrackId() === trackId);
+    if (au) {
+        res.send(au);
     } else {
         res.send({});
     }
 });
 
-app.get('/users/:id/score', (req: Request, res: Response) => {
-    const id = Number(req.params.id);
 
-    const user = array.find(user => user.getId() === id);
-    if (user) {
-        res.send(user.getScore());
-    } else {
-        res.send('not found');
+
+app.get('/PlayList//:artistName', (req: Request, res: Response) => {
+   const artistName = req.params.artistName;
+   const au = array.filter(au => au.getArtistName() === artistName);
+   if (au.length == 0){
+    res.send('not found'); }
+    else {
+       if (au) {
+           res.send(au);
+         }
+       else {
+       res.send('not found error');
+      }
     }
 });
 
-app.post('/users/:id/score', (req: Request, res: Response) => {
-    const id = Number(req.params.id);
-
-    const user = array.find(user => user.getId() === id);
-    if (user) {
-        user.addScore(req.body.value);
-        res.send('DONE!');
-    } else {
-        res.send('not found');
+app.put('/PlayList/:trackId', (req: Request, res: Response) => {
+    const trackId = Number(req.params.trackId);
+    const autsong = array.find(autsong => autsong.getTrackId() === trackId);
+   if (autsong) {
+            autsong.setArtistName(req.body.artistName);
+            autsong.setSongName(req.body.songName);
+            autsong.setPlaytime(Number(req.body.playtime));
+            autsong.setAdrUrl(req.body.adrUrl);
+            res.send(autsong);
     }
+    else {
+      res.send('autsong not found!');
+   }
 });
 
-app.put('/users/:id', (req: Request, res: Response) => {
-    const id = Number(req.params.id);
-
-    const user = array.find(user => user.getId() === id);
-    if (user) {
-        if (req.body.name?.length >= 3 && req.body.age !== undefined) {
-
-            user.setName(req.body.name);
-            user.setAge(req.body.age);
-
-            res.send(`OK - user with id: ${user.getId()} UPDATED!`);
-        } else {
-            res.send(`Invalid value!`);
-        }
-    } else {
-        res.send('User not found!');
-    }
+app.delete('/PlayList/:trackId', (req: Request, res: Response) => {
+    const trackId = Number(req.params.trackId);
+    array = array.filter(autsong => autsong.getTrackId() !== trackId);
+    res.send(`TrackId: ${trackId} DELETED!`);
 });
 
-app.delete('/users/:id', (req: Request, res: Response) => {
-    const id = Number(req.params.id);
+app.post('/PlayList', (req: Request, res: Response) => {
 
-    array = array.filter(user => user.getId() !== id);
-    res.send(`Id: ${id} DELETED!`);
-});
+       const pl= new PlayList( array.length + 1,
+        req.body.artistName,
+        req.body.songName,
+        Number(req.body.playtime),
+        req.body.adrUrl
+        );
+         array.push(pl);
+         res.send(pl);
 
-app.post('/users', (req: Request, res: Response) => {
-    if (req.body.name?.length >= 3 && req.body.age !== undefined) {
-        const id = array.length + 1;
-        const value = new Person(id ,req.body.name, req.body.age);
-        const index = array.push(value);
-        res.send(`OK - new id: ${id}`);
-    } else {
-        res.send(`Invalid value!`);
-    }
+        res.send(`OK - new trackId: ${array.length + 1}`);
 });
 
 app.listen(port, () => {
