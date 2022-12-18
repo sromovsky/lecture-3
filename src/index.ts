d7bd3945a1861c4df58a49201973ff6ceaf5b9fe
uzivk <uzivkonta@gmail.com>
2022-12-18 18:02:51 +0100

    lecture 3

--- src/index.ts
+++ src/index.ts
@@ -37,96 +37,18 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.get('/choice', (req: Request, res: Response) => {
-    const score = Number(req.query.withScore);
+    let result = array;
 
-    console.log(score);
-
-    let result;
-
-    if (score) {
-        result = array.filter(user => user.getScore().includes(score))
-    } else {
-        result = array;
-    }
-
-    res.send(result.map(user => {
+    res.send(result.map(Fighter => {
         return {
-            id: user.getId(),
-            name: user.getName()
+            name: Fighter.getName(),
+            age: Fighter.getAge(),
+            nationality: Fighter.getNationality(),
+            Weight: Fighter.getWeight()
         }
     }));
 });
 
-app.get('/users/:id', (req: Request, res: Response) => {
-    const id = Number(req.params.id);
-
-    const user = array.find(user => user.getId() === id);
-    if (user) {
-        res.send(user);
-    } else {
-        res.send({});
-    }
-});
-
-app.get('/users/:id/score', (req: Request, res: Response) => {
-    const id = Number(req.params.id);
-
-    const user = array.find(user => user.getId() === id);
-    if (user) {
-        res.send(user.getScore());
-    } else {
-        res.send('not found');
-    }
-});
-
-app.post('/users/:id/score', (req: Request, res: Response) => {
-    const id = Number(req.params.id);
-
-    const user = array.find(user => user.getId() === id);
-    if (user) {
-        user.addScore(req.body.value);
-        res.send('DONE!');
-    } else {
-        res.send('not found');
-    }
-});
-
-app.put('/users/:id', (req: Request, res: Response) => {
-    const id = Number(req.params.id);
-
-    const user = array.find(user => user.getId() === id);
-    if (user) {
-        if (req.body.name?.length >= 3 && req.body.age !== undefined) {
-
-            user.setName(req.body.name);
-            user.setAge(req.body.age);
-
-            res.send(`OK - user with id: ${user.getId()} UPDATED!`);
-        } else {
-            res.send(`Invalid value!`);
-        }
-    } else {
-        res.send('User not found!');
-    }
-});
-
-app.delete('/users/:id', (req: Request, res: Response) => {
-    const id = Number(req.params.id);
-
-    array = array.filter(user => user.getId() !== id);
-    res.send(`Id: ${id} DELETED!`);
-});
-
-app.post('/users', (req: Request, res: Response) => {
-    if (req.body.name?.length >= 3 && req.body.age !== undefined) {
-        const id = array.length + 1;
-        const value = new Person(id ,req.body.name, req.body.age);
-        const index = array.push(value);
-        res.send(`OK - new id: ${id}`);
-    } else {
-        res.send(`Invalid value!`);
-    }
-});
 
 app.listen(port, () => {
     console.log(`⚡ [server]: Server is running at http://localhost:${port}`)
